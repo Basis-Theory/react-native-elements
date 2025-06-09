43dd58d286154556439274379c0868d2132f9bee
washluis-alencar <wluisalencar@gmail.com>
2025-06-09 11:29:42 -0300

    feat(eng-8287): add encryption method to create jwe

--- src/utils/tokenEncryption.ts
+++ src/utils/tokenEncryption.ts
@@ -5,7 +5,7 @@ const ENCRYPTION = {
   CURVE: 'X25519',
   ALGORITHM: 'ECDH-ES',
   ENCRYPTION_ALGORITHM: 'A256GCM'
-};
+} as const;
 
 export class TokenEncryption {
   private readonly publicKey: string;
@@ -18,26 +18,34 @@ export class TokenEncryption {
     const jwk: JWK = {
       kty: ENCRYPTION.KEY_TYPE,
       crv: ENCRYPTION.CURVE,
-      x: this.publicKey
+      x: this.publicKey,
+      kid: "db40a40a-9097-45ff-b353-0488a6a4d193"
     };
 
     const key = await importJWK(jwk, ENCRYPTION.ALGORITHM);
-    
+
     return new CompactEncrypt(
       new TextEncoder().encode(payload)
     )
-      .setProtectedHeader({ alg: ENCRYPTION.ALGORITHM, enc: ENCRYPTION.ENCRYPTION_ALGORITHM })
+      .setProtectedHeader({ alg: ENCRYPTION.ALGORITHM, enc: ENCRYPTION.ENCRYPTION_ALGORITHM, kid: jwk.kid })
       .encrypt(key);
   }
 }
 
 // Example usage
 async function main(): Promise<void> {
-  const rawPublicKey = "uIXXIyiF537fwvxEcPwFiSYgtD7y3p2KswRAqrwkPjI=";
+  const rawPublicKey = "TUUclxBySPBHt8YLB6SqXSAjXWB2dy4NoAQpt833GAw=";
   const encryptor = new TokenEncryption(rawPublicKey);
-  
-  const jwe = await encryptor.encrypt("Encrypted Text!");
+
+  const jwe = await encryptor.encrypt(JSON.stringify(
+    {
+      "number": "4200000000000000",
+      "expiration_month": 12,
+      "expiration_year": 2025,
+      "cvc": "123"
+    }
+  ));
   console.log(jwe);
 }
 
-main().catch(console.error); 
+main().catch(console.error);
