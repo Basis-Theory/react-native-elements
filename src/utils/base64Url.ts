8c4fd29f1ff75bcc2937f48981359649db983153
washluis-alencar <wluisalencar@gmail.com>
2025-06-17 11:23:17 -0300

    feat(eng-8294): refactoring jwe.ts to use IIFE and revealing module pattern

--- src/utils/base64Url.ts
+++ src/utils/base64Url.ts
@@ -5,38 +5,45 @@
  * - Uses '_' instead of '/'
  * - Removes padding '=' characters
  */
-export class Base64Url {
+export const Base64Url = (() => {
   /**
    * Encode a Uint8Array to base64url string
    */
-  static encode(input: Uint8Array): string {
+  const encode = (input: Uint8Array): string => {
     const base64 = btoa(String.fromCharCode(...input));
     return base64.replace(/\+/g, '-').replace(/\//g, '_').replace(/=/g, '');
-  }
+  };
 
   /**
    * Decode a base64url string to Uint8Array
    */
-  static decode(input: string): Uint8Array {
+  const decode = (input: string): Uint8Array => {
     const base64 = input.replace(/-/g, '+').replace(/_/g, '/');
     const padded = base64 + '='.repeat((4 - base64.length % 4) % 4);
     const decoded = atob(padded);
     return new Uint8Array(decoded.split('').map(char => char.charCodeAt(0)));
-  }
+  };
 
   /**
    * Encode a string to base64url
    */
-  static encodeString(input: string): string {
+  const encodeString = (input: string): string => {
     const bytes = new TextEncoder().encode(input);
-    return Base64Url.encode(bytes);
-  }
+    return encode(bytes);
+  };
 
   /**
    * Decode a base64url string to string
    */
-  static decodeString(input: string): string {
-    const bytes = Base64Url.decode(input);
+  const decodeString = (input: string): string => {
+    const bytes = decode(input);
     return new TextDecoder().decode(bytes);
-  }
-} 
+  };
+
+  return {
+    encode,
+    decode,
+    encodeString,
+    decodeString
+  };
+})(); 
