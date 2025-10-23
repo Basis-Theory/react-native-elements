f9098da3957d53458ce23c3c8ae57e8133cf01d1
Kevin Peraza <kevin@basistheory.com>
2025-10-23 13:51:05 -0600

    fix: export bt provider from index (#29)

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ export { CardNumberElement } from './components/CardNumberElement';
 export { CardVerificationCodeElement } from './components/CardVerificationCodeElement';
 export { CardExpirationDateElement } from './components/CardExpirationDateElement';
 export { TextElement } from './components/TextElement';
+export { BasisTheoryProvider } from './BasisTheoryProvider';
 export { useBasisTheory } from './useBasisTheory';
 export type { BTRef, BTDateRef, ElementEvent } from './BaseElementTypes';
 export type { Token } from '@basis-theory/basis-theory-js/types/models';
