d56e2fed230f9540f9e2a30de2273bc52cd4b996
Diego Diestra <ddiestras@gmail.com>
2025-11-13 15:50:12 -0500

    chore: fix types issues.

--- src/services/api/mappers/tokens/toUpdateTokenRequest.ts
+++ src/services/api/mappers/tokens/toUpdateTokenRequest.ts
@@ -1,8 +1,10 @@
 import { UpdateToken } from '../../../../types';
 import { convertToSnakeCase } from '../case-conversion-utils';
-import { isUpdateToken } from './type-guards';
+import { isUpdateToken, UpdateTokenResponse } from './type-guards';
 
-export const toUpdateTokenRequest = (payload: Partial<UpdateToken>) => {
+export const toUpdateTokenRequest = (
+  payload: Partial<UpdateToken>
+): UpdateTokenResponse | undefined => {
   const _payload = convertToSnakeCase(payload);
 
   return isUpdateToken(_payload)
